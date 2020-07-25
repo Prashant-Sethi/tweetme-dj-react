21efcbd3dfac04e95fb60c9eff4769ce2aee0f89
Prashant Sethi <theseth.prashant@gmail.com>
2020-07-26 01:51:27 +0530

    Created Profile Model and handle missing profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .models import Profile
 
 
 def profile_detail_view(request, username, *args, **kwargs):
-    # get the profile for the passed username
+    get_object_or_404(Profile, user__username=username)
     return render(request, "pages/react.html")
