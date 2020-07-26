e4b748aefb37fb3412c961e0bea8df50f1cf7510
Prashant Sethi <theseth.prashant@gmail.com>
2020-07-26 21:39:09 +0530

    Updating profile

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import profile_detail_view
+from .views import profile_detail_view, profile_update_view
 
 urlpatterns = [
+    path('update/', profile_update_view, name='profile-update-view'),
     path('<str:username>/', profile_detail_view, name='profile-view'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,40 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 
 # Create your views here.
 from .models import Profile
+from .forms import ProfileForm
 
 
 def profile_detail_view(request, username, *args, **kwargs):
     get_object_or_404(Profile, user__username=username)
     return render(request, "pages/react.html")
+
+
+def profile_update_view(request, *args, **kwargs):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    user = request.user
+    user_data = {
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'email': user.email,
+    }
+    profile = user.profile
+    form = ProfileForm(request.POST or None,
+                       instance=profile, initial=user_data)
+    if form.is_valid():
+        profile_obj = form.save(commit=False)
+        form_data = form.cleaned_data
+        user.first_name = form_data.get('first_name')
+        user.last_name = form_data.get('last_name')
+        user.email = form_data.get('email')
+        profile_obj.save()
+        user.save()
+    title = f'Hello, {f"{user.first_name} {user.last_name}" if user.first_name else user.username}'
+    context = {
+        'form': form,
+        'btn_label': 'Save',
+        'title': title,
+        'description': 'Update Your Profile'
+    }
+    return render(request, 'profiles/update.html', context)
